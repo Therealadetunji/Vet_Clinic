0c1d608d73798fcab9dadc407bcb5bdcddefabf6
Michael <madetunji1376@gmail.com>
2022-09-20 20:22:24 +0100

    schema, data and query done

--- queries.sql
+++ queries.sql
@@ -1,3 +1,8 @@
 /*Queries that provide answers to the questions from all projects.*/
 
-SELECT * from animals WHERE name = 'Luna';
+SELECT * FROM animals WHERE animal_name LIKE '%mon%';
+SELECT * FROM animals WHERE neutered = '1' AND escape_attempts < 3;
+SELECT * FROM animals WHERE weight_kg > 10.5;
+SELECT * FROM animals WHERE neutered = '1';
+SELECT * FROM animals WHERE animal_name IN ('Agumon', 'Pikachu', 'Devimon');
+SELECT * FROM animals WHERE weight_kg >= 10.4 AND weight_kg <= 17.3;
